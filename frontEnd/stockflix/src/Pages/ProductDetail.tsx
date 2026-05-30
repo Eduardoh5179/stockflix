cb820754a0fd0a32c88ee042f4cf17ea48544131
Eduardoh5179 <eduardorsim7@gmail.com>
2026-05-30 12:25:40 -0300

    feat: animação de loading para o productDetail

--- frontEnd/stockflix/src/Pages/ProductDetail.tsx
+++ frontEnd/stockflix/src/Pages/ProductDetail.tsx
@@ -70,140 +70,175 @@ const ProductDetail = () => {
                     <Header onMenuClick={() => setsidebarOpen(!sidebarOpen)} />
                     <Sidebar isOpen={sidebarOpen} />
                     <main className='h-full flex-1'>
-                        <section className={`${sidebarOpen ? 'ml-64' : 'ml-0'} transition-all duration-300 p-6`}>
-                            <h2> Carregando dados.... </h2>
+                        <section className={`${sidebarOpen ? 'md:ml-64' : 'md:ml-0'} transition-all duration-300 p-6`}>
+                            <div className="bg-white border border-(--borderColor)">
+
+                                    <>
+                                        <section className="p-8 border-b border-(--borderColor) flex flex-col gap-4 animate-pulse">
+                                            <div className="bg-zinc-200 h-4 w-16 rounded-md"></div>
+
+                                            <div className="flex justify-between items-center">
+                                                <div className="bg-zinc-200 h-8 w-1/2 md:w-1/3 rounded-md"></div>
+                                                <div className="bg-zinc-200 h-5 w-14 rounded-md"></div>
+                                            </div>
+
+                                            <div className="bg-zinc-200 h-10 w-48 rounded-md mt-2"></div>
+                                        </section>
+
+                                        <section className="p-10 flex flex-col gap-6 animate-pulse">
+                                            <div className="flex flex-col gap-2">
+                                                <div className="bg-zinc-200 h-4 w-full rounded-md"></div>
+                                                <div className="bg-zinc-200 h-4 w-5/6 rounded-md"></div>
+                                            </div>
+
+                                            <div className="border-t border-(--borderColor) pt-2 flex flex-col gap-4">
+                                                {Array.from({ length: 4 }).map((_, index) => (
+                                                    <div key={`skeleton-row-${index}`} className="flex justify-between py-2 px-4 border-b border-(--borderColor)/50">
+                                                        <div className="bg-zinc-200 h-4 w-28 rounded-md"></div>
+                                                        <div className="bg-zinc-200 h-4 w-16 rounded-md"></div>
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        </section>
+
+                                        <section className="border-t border-(--borderColor) p-8 animate-pulse">
+                                            <div className="bg-zinc-200 h-12 w-full rounded-md"></div>
+                                        </section>
+                                    </>
+                            </div>
                         </section>
                     </main>
+            
                 </>
             );
         }
 
     }
 
-    if (!produto) {
-        return (
-            <>
-                <Header onMenuClick={() => setsidebarOpen(!sidebarOpen)} />
-                <Sidebar isOpen={sidebarOpen} />
-                <main className='h-full flex-1'>
-                    <section className={`${sidebarOpen ? 'ml-64' : 'ml-0'} transition-all duration-300 p-6`}>
-                        <h2> ERRO </h2>
-                    </section>
-                </main>
-            </>
-        );
-    }
-    const valorEmEstoque = produto.preco * produto.quantidade
-
-    const atualizarEstoquePai = (novaQuantidade: number) => {
-        if (produto) {
-            setProduto({ ...produto, quantidade: novaQuantidade });
-        }
-    };
-
-
+if (!produto) {
     return (
         <>
-            <div className="flex flex-col min-h-screen">
-                <Header onMenuClick={() => setsidebarOpen(!sidebarOpen)} />
-                <Sidebar isOpen={sidebarOpen} />
-                <main className='h-full flex-1'>
-                    <section className={`${sidebarOpen ? 'md:ml-64' : 'md:ml-0'} transition-all duration-300 p-6`}>
-                        <div className="bg-white border border-(--borderColor)">
-                            <section className="p-8 border-b border-(--borderColor)">
-
-                                <span className="text-zinc-500 text-sm">ID: {id}</span>
-                                <section className="flex justify-between">
-                                    {isEditing && user?.acessoADM ? (
-                                        <input name="nome" className="text-xl md:text-3xl font-bold text-slate-800 mb-4 border-b-2 border-violet-500 outline-none bg-slate-50 px-2 w-full" value={editForm?.nome} onChange={handleChange} />) : (
-                                        <h1 className="text-xl md:text-3xl font-bold text-slate-800 mb-4">{produto.nome}</h1>
-                                    )}
-                                    <p>Status</p>
-                                </section>
-                                <section>
-                                    <p className="text-4xl font-bold">{produto.quantidade} <span className="font-medium text-xl">em estoque</span></p>
-                                </section>
-                            </section>
-
-                            <section className="p-10 font-sans flex flex-col gap-4">
-                                {isEditing && user?.acessoADM ? (
-                                    <textarea name="descricao" className="w-full p-3 border border-violet-300 rounded-md text-sm bg-slate-50 outline-none" rows={3} value={editForm?.descricao} onChange={handleChange} />) : (
-                                    <p className="text-sm">{produto.descricao}</p>
-                                )}
-                                <section className="font-serif ">
-                                    <div className="border-y flex font-medium justify-between border-(--borderColor) py-2 px-4">
-                                        <p>estoque previsto</p>
-                                        <p>1</p>
-                                    </div>
-                                    <div className="border-b flex font-medium justify-between border-(--borderColor) py-2 px-4">
-                                        <p>Setor</p>
-                                        {isEditing && user?.acessoADM ? (<select name="setorId" value={editForm?.setorId} onChange={handleChange} className="border border-violet-300 rounded px-2 py-1 text-sm outline-none">
-                                            <option value={1}>Setor 1</option>
-                                            <option value={2}>Setor 2</option>
-                                            <option value={3}>Setor 3</option>
-                                        </select>) : (
-                                            <p>{produto.setorId}</p>)}
-                                    </div>
-                                    <div className="border-b flex font-medium justify-between border-(--borderColor) py-2 px-4">
-                                        <p>valor unitário</p>
-                                        {isEditing && user?.acessoADM ? (
-                                            <div className="flex items-center gap-1">
-                                                <span className="text-sm text-slate-50">R$</span>
-                                                <input name="preco" type="number" step="0.01" className="border border-violet-300 rounded px-2 py-1 text-sm w-24 outline-none" value={editForm?.preco} onChange={handleChange} />
-                                            </div>) : (<p>{produto.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>)}
-                                    </div>
-                                    <div className="border-b flex font-medium justify-between border-(--borderColor) py-2 px-4">
-                                        <p>valor em estoque</p>
-                                        <p>{valorEmEstoque.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
-                                    </div>
-                                </section>
-                            </section>
-                            <section className="border-t border-(--borderColor) p-8 ">
-                                <Movement produtoAtual={produto} onUpdate={atualizarEstoquePai} />
-                            </section>
-
-                            {user?.acessoADM === true && (
-                                <section className="border-t border-(--borderColor) flex items-center justify-center p-6 gap-3">
-                                    {isEditing ? (
-                                        <>
-                                            <button onClick={() => setIsEditing(false)} className="px-4 py-2 text-sm font-medium border border-zinc-300 rounded-md hover:bg-zinc-50 transition-colors cursor-pointer">
-                                                Cancelar
-                                            </button>
-                                            <button
-                                                onClick={async () => {
-                                                    if (!editForm) return;
-                                                    setIsUpdating(true);
-                                                    try {
-                                                        const resultado = await atualizarProduto(Number(id), editForm);
-                                                        setProduto(resultado);
-                                                        setIsEditing(false);
-                                                    } catch (error) {
-                                                        alert("Erro ao salvar alterações.");
-                                                    } finally {
-                                                        setIsUpdating(false);
-                                                    }
-                                                }}
-                                                disabled={isUpdating}
-                                                className="px-4 py-2 text-sm font-medium bg-violet-700 text-white rounded-md hover:bg-violet-800 disabled:opacity-50 flex items-center gap-2 cursor-pointer"
-                                            >
-                                                {isUpdating && <Spinner className="h-4 w-4 animate-spin" />}
-                                                Salvar Alterações
-                                            </button>
-                                        </>
-                                    ) : (
-                                        <button onClick={handleStartEditing} className="px-6 py-2 border border-zinc-400 cursor-pointer rounded-md hover:bg-slate-50 transition-all font-medium text-slate-700">
-                                            Editar Informações
-                                        </button>
-                                    )}
-                                </section>
-                            )}
-                        </div>
-                    </section>
-                </main>
-                <Footer />
-            </div>
+            <Header onMenuClick={() => setsidebarOpen(!sidebarOpen)} />
+            <Sidebar isOpen={sidebarOpen} />
+            <main className='h-full flex-1'>
+                <section className={`${sidebarOpen ? 'ml-64' : 'ml-0'} transition-all duration-300 p-6`}>
+                    <h2> ERRO </h2>
+                </section>
+            </main>
         </>
-    )
+    );
+}
+const valorEmEstoque = produto.preco * produto.quantidade
+
+const atualizarEstoquePai = (novaQuantidade: number) => {
+    if (produto) {
+        setProduto({ ...produto, quantidade: novaQuantidade });
+    }
+};
+
+
+return (
+    <>
+        <div className="flex flex-col min-h-screen">
+            <Header onMenuClick={() => setsidebarOpen(!sidebarOpen)} />
+            <Sidebar isOpen={sidebarOpen} />
+            <main className='h-full flex-1'>
+                <section className={`${sidebarOpen ? 'md:ml-64' : 'md:ml-0'} transition-all duration-300 p-6`}>
+                    <div className="bg-white border border-(--borderColor)">
+                        <section className="p-8 border-b border-(--borderColor)">
+
+                            <span className="text-zinc-500 text-sm">ID: {id}</span>
+                            <section className="flex justify-between">
+                                {isEditing && user?.acessoADM ? (
+                                    <input name="nome" className="text-xl md:text-3xl font-bold text-slate-800 mb-4 border-b-2 border-violet-500 outline-none bg-slate-50 px-2 w-full" value={editForm?.nome} onChange={handleChange} />) : (
+                                    <h1 className="text-xl md:text-3xl font-bold text-slate-800 mb-4">{produto.nome}</h1>
+                                )}
+                                <p>Status</p>
+                            </section>
+                            <section>
+                                <p className="text-4xl font-bold">{produto.quantidade} <span className="font-medium text-xl">em estoque</span></p>
+                            </section>
+                        </section>
+
+                        <section className="p-10 font-sans flex flex-col gap-4">
+                            {isEditing && user?.acessoADM ? (
+                                <textarea name="descricao" className="w-full p-3 border border-violet-300 rounded-md text-sm bg-slate-50 outline-none" rows={3} value={editForm?.descricao} onChange={handleChange} />) : (
+                                <p className="text-sm">{produto.descricao}</p>
+                            )}
+                            <section className="font-serif ">
+                                <div className="border-y flex font-medium justify-between border-(--borderColor) py-2 px-4">
+                                    <p>estoque previsto</p>
+                                    <p>1</p>
+                                </div>
+                                <div className="border-b flex font-medium justify-between border-(--borderColor) py-2 px-4">
+                                    <p>Setor</p>
+                                    {isEditing && user?.acessoADM ? (<select name="setorId" value={editForm?.setorId} onChange={handleChange} className="border border-violet-300 rounded px-2 py-1 text-sm outline-none">
+                                        <option value={1}>Setor 1</option>
+                                        <option value={2}>Setor 2</option>
+                                        <option value={3}>Setor 3</option>
+                                    </select>) : (
+                                        <p>{produto.setorId}</p>)}
+                                </div>
+                                <div className="border-b flex font-medium justify-between border-(--borderColor) py-2 px-4">
+                                    <p>valor unitário</p>
+                                    {isEditing && user?.acessoADM ? (
+                                        <div className="flex items-center gap-1">
+                                            <span className="text-sm text-slate-50">R$</span>
+                                            <input name="preco" type="number" step="0.01" className="border border-violet-300 rounded px-2 py-1 text-sm w-24 outline-none" value={editForm?.preco} onChange={handleChange} />
+                                        </div>) : (<p>{produto.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>)}
+                                </div>
+                                <div className="border-b flex font-medium justify-between border-(--borderColor) py-2 px-4">
+                                    <p>valor em estoque</p>
+                                    <p>{valorEmEstoque.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+                                </div>
+                            </section>
+                        </section>
+                        <section className="border-t border-(--borderColor) p-8 ">
+                            <Movement produtoAtual={produto} onUpdate={atualizarEstoquePai} />
+                        </section>
+
+                        {user?.acessoADM === true && (
+                            <section className="border-t border-(--borderColor) flex items-center justify-center p-6 gap-3">
+                                {isEditing ? (
+                                    <>
+                                        <button onClick={() => setIsEditing(false)} className="px-4 py-2 text-sm font-medium border border-zinc-300 rounded-md hover:bg-zinc-50 transition-colors cursor-pointer">
+                                            Cancelar
+                                        </button>
+                                        <button
+                                            onClick={async () => {
+                                                if (!editForm) return;
+                                                setIsUpdating(true);
+                                                try {
+                                                    const resultado = await atualizarProduto(Number(id), editForm);
+                                                    setProduto(resultado);
+                                                    setIsEditing(false);
+                                                } catch (error) {
+                                                    alert("Erro ao salvar alterações.");
+                                                } finally {
+                                                    setIsUpdating(false);
+                                                }
+                                            }}
+                                            disabled={isUpdating}
+                                            className="px-4 py-2 text-sm font-medium bg-violet-700 text-white rounded-md hover:bg-violet-800 disabled:opacity-50 flex items-center gap-2 cursor-pointer"
+                                        >
+                                            {isUpdating && <Spinner className="h-4 w-4 animate-spin" />}
+                                            Salvar Alterações
+                                        </button>
+                                    </>
+                                ) : (
+                                    <button onClick={handleStartEditing} className="px-6 py-2 border border-zinc-400 cursor-pointer rounded-md hover:bg-slate-50 transition-all font-medium text-slate-700">
+                                        Editar Informações
+                                    </button>
+                                )}
+                            </section>
+                        )}
+                    </div>
+                </section>
+            </main>
+            <Footer />
+        </div>
+    </>
+)
 }
 
 export default ProductDetail
